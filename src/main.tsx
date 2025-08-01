91de9821da3e1c356370965ed134ed83b05682fc
aws-test5060 <aws-github-test5060@outlook.com>
2025-08-01 18:45:44 +0900

    Update main.tsx
    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,8 @@ Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Authenticator>
     <App />
+    </Authenticator>
   </React.StrictMode>
 );
